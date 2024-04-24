4d22e6e0b4ca86f538ba1fd1fb41f53b9156b23e
Abdul Fatah <fatah03111995@gmail.com>
2024-04-24 15:24:08 +0700

    feat : add slider

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,8 @@
 import 'package:bmi_medium/components/box.dart';
 import 'package:bmi_medium/components/icon_text.dart';
+import 'package:bmi_medium/constants/color.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-const double bottomContainerHeight = 80.0;
-const Color darkPurple1 = Color.fromRGBO(36, 37, 65, 1);
-const Color darkPurple2 = Color(0XFF1D1E33);
-const Color darkPink = Color(0xffeb1555);
 
 enum Gender { male, female }
 
@@ -17,10 +13,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
-  Color activeButton = darkPurple2;
-  Color inActiveButton = darkPurple1;
-
+  Color activeButtonMale = blue;
+  Color activeButtonFemale = darkPink;
+  Color inActiveButton = purple;
   Gender? isActive;
+  double heightBody = 0;
 
   void buttonActive(Gender gender) {
     setState(() {
@@ -38,13 +35,15 @@ class _HomePage extends State<HomePage> {
         centerTitle: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: [
                 Box(
-                  color:
-                      isActive == Gender.male ? activeButton : inActiveButton,
+                  color: isActive == Gender.male
+                      ? activeButtonMale
+                      : inActiveButton,
                   boxChild: IconText(
                     onTap: () {
                       buttonActive(Gender.male);
@@ -54,8 +53,9 @@ class _HomePage extends State<HomePage> {
                   ),
                 ),
                 Box(
-                  color:
-                      isActive == Gender.female ? activeButton : inActiveButton,
+                  color: isActive == Gender.female
+                      ? activeButtonFemale
+                      : inActiveButton,
                   boxChild: IconText(
                     onTap: () {
                       buttonActive(Gender.female);
@@ -67,28 +67,59 @@ class _HomePage extends State<HomePage> {
               ],
             ),
           ),
-          const Expanded(
+          Expanded(
             child: Box(
-              color: darkPurple1,
-              boxChild: null,
+              color: purple,
+              boxChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('HEIGHT'),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        heightBody.toString(),
+                        style: const TextStyle(fontWeight: FontWeight.w800),
+                      ),
+                      const Text(' cm')
+                    ],
+                  ),
+                  Slider(
+                      value: heightBody,
+                      min: 0,
+                      max: 200,
+                      divisions: 200,
+                      activeColor: lightPink,
+                      thumbColor: darkPink,
+                      onChanged: (double newVal) {
+                        setState(() {
+                          heightBody = newVal.roundToDouble();
+                        });
+                      })
+                ],
+              ),
             ),
           ),
           const Expanded(
             child: Row(
               children: [
                 Box(
-                  color: darkPurple1,
+                  color: purple,
                   boxChild: null,
                 ),
                 Box(
-                  color: darkPurple1,
+                  color: purple,
                   boxChild: null,
                 ),
               ],
             ),
           ),
           Container(
-            height: bottomContainerHeight,
+            height: 80.0,
             width: double.infinity,
             decoration: const BoxDecoration(
               color: darkPink,
