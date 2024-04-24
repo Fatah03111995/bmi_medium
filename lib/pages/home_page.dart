75ac02e505ced468d5d962e13bab6d9924b9418d
Abdul Fatah <fatah03111995@gmail.com>
2024-04-24 14:02:04 +0700

    feat : add func to choice the gender

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,8 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double bottomContainerHeight = 80.0;
-const Color darkPurple = Color(0XFF1D1E33);
+const Color darkPurple1 = Color.fromRGBO(36, 37, 65, 1);
+const Color darkPurple2 = Color(0XFF1D1E33);
 const Color darkPink = Color(0xffeb1555);
+
+enum Gender { male, female }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,6 +17,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  Color activeButton = darkPurple2;
+  Color inActiveButton = darkPurple1;
+
+  Gender? isActive;
+
+  void buttonActive(Gender gender) {
+    setState(() {
+      isActive = gender;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,19 +39,27 @@ class _HomePage extends State<HomePage> {
       ),
       body: Column(
         children: [
-          const Expanded(
+          Expanded(
             child: Row(
               children: [
                 Box(
-                  color: darkPurple,
+                  color:
+                      isActive == Gender.male ? activeButton : inActiveButton,
                   boxChild: IconText(
+                    onTap: () {
+                      buttonActive(Gender.male);
+                    },
                     icon: FontAwesomeIcons.mars,
                     txt: 'MALE',
                   ),
                 ),
                 Box(
-                  color: darkPurple,
+                  color:
+                      isActive == Gender.female ? activeButton : inActiveButton,
                   boxChild: IconText(
+                    onTap: () {
+                      buttonActive(Gender.female);
+                    },
                     icon: FontAwesomeIcons.venus,
                     txt: 'FEMALE',
                   ),
@@ -47,7 +69,7 @@ class _HomePage extends State<HomePage> {
           ),
           const Expanded(
             child: Box(
-              color: darkPurple,
+              color: darkPurple1,
               boxChild: null,
             ),
           ),
@@ -55,11 +77,11 @@ class _HomePage extends State<HomePage> {
             child: Row(
               children: [
                 Box(
-                  color: darkPurple,
+                  color: darkPurple1,
                   boxChild: null,
                 ),
                 Box(
-                  color: darkPurple,
+                  color: darkPurple1,
                   boxChild: null,
                 ),
               ],
