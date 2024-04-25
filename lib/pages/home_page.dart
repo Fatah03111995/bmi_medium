083118da3c3a055892f8caa6d175e83f6cf09b01
Abdul Fatah <fatah03111995@gmail.com>
2024-04-25 13:08:18 +0700

    fix : minus button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -137,7 +137,9 @@ class _HomePage extends State<HomePage> {
                           const SizedBox(width: 10),
                           RoundButton(
                             onTap: () {
-                              weightBody--;
+                              setState(() {
+                                weightBody--;
+                              });
                             },
                             icon: FontAwesomeIcons.minus,
                             colorIcon: primaryColor,
